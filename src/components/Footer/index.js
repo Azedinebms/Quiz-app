7d3db3b055ce47021befdb169e6bbdd239ce9f96
AIT BAMOUSSA Azedine <bamoussa.azedine@gmail.com>
2023-02-07 23:25:18 +0100

    Création des composonts Header Landing et Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+
+
+
+  return (
+   <footer>
+    <div className='footer-container'>
+        <p>Projet réaliser par recTech</p>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
